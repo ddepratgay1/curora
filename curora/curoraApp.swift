242e3066813c1e951a3101afb9765e89d89ada76
Delfina de Prat Gay <ddepratgay1@babson.edu>
2026-04-07 17:14:53 -0400

    Initial Commit

---
+++ curora/curoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  curoraApp.swift
+//  curora
+//
+//  Created by Delfina de Prat Gay on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct curoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
